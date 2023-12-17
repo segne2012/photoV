935fc0c7832f27b783926396bacbfe7d67bbb89c
segne2012 <97039021@qq.com>
2023-12-17 15:39:18 +0800

    增加事件

--- src/components/imageView.vue
+++ src/components/imageView.vue
@@ -8,8 +8,17 @@ const props = defineProps({
     }
 })
 
+const imageView = ref<null | HTMLDivElement>(null);
 const sytleObj = reactive({
+    scale: 1,
+    x: 0,
+    y: 0
+})
 
+const styles = computed(() => {
+    return {
+        transform: `scale(${styleObj.scale}) translateX(${styleObj.x}px) translateY(${styleObj.y}px)`
+    }
 })
 
 const mousewheel = (event: WheelEvent) => {
@@ -21,11 +30,65 @@ const mousewheel = (event: WheelEvent) => {
         console.log("Scrolling up")
     }
 }
+
+const hasMouseDwon = ref(false)
+const mousePosition = reactive({
+    x: 0,
+    y: 0
+})
+const mouseDown = (event: MouseEvent) => {
+    hasMouseDwon.value = true;
+    mousePosition.x = event.x;
+    mousePosition.y = event.y;
+}
+const mousemove = (event: MouseEvent) => {
+
+    if(hasMouseDwon) {
+
+        styleObj.x = mousePosition.x - event.x;
+        styleObj.y = mousePosition.y - event.y;
+    }
+}
+
+const mouseUp = (event: MouseEvent) => {
+    hasMouseDwon.value = false;
+}
+
+const checkBounds = (event: CheckBoundsEvent) => {
+    if (event.x < 0) {
+        styleObj.x = 0;
+    } else if (event.x > imageView.value!.clientWidth) {
+        styleObj.x = imageView.value!.clientWidth;
+    }
+    if (event.y < 0) {
+        styleObj.y = 0;
+    } else if (event.y > imageView.value!.clientHeight) {
+        styleObj.y = imageView.value!.clientHeight;
+    }
+}
+
+onMounted(() => {
+    if(imageView.value) {
+        imageView.value.addEventListener('wheel', mousewheel);
+        imageView.value.addEventListener('mousedown', mouseDown);
+        imageView.value.addEventListener('mousemove', mousemove);
+        imageView.value.addEventListener('mouseup', mouseUp);
+    }
+})
+
+onUnmounted(() => {
+    if(imageView.value) {
+        imageView.value.removeEventListener('wheel', mousewheel);
+        imageView.value.removeEventListener('mousedown', mouseDown);
+        imageView.value.removeEventListener('mousemove', mousemove);
+        imageView.value.removeEventListener('mouseup', mouseUp);
+    }
+})
 </script>
 
 <template>
-    <div class="image-view" @mousewheel="mousewheel">
-        <div class="box" :style="sytleObj">
+    <div class="image-view" ref="imageView" @mousewheel="mousewheel">
+        <div class="box" :style="styles">
             <img :src="props.showUrl" />
         </div>
     </div>
